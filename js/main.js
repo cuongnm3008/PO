edf69986f9e22ff7f941d1c7756e4b977a5f697f
ManhCuong3008 <cuongnguyenmanh3008@gmail.com>
2022-03-23 13:11:01 +0700

    new

--- js/main.js
+++ js/main.js
@@ -68,21 +68,38 @@ $(document).ready(function () {
     });
 });
 
+// //Tab menu Navigation
+// $(document).ready(function(){
+//     // set defaut active for tab navigation first 
+//     $('.tab_container:first').show();
+//     $('.tab_navigation li:first div').addClass('active');
+
+//     // set defaut active for tab navigation was clicked        
+//     $('.tab_navigation li').click(function(event){
+//         index  = $(this).index();
+//         $('.tab_navigation li div').removeClass('active');
+//         $(this).children('div').eq(0).addClass('active');
+//         $('.tab_container').hide();
+//         $('.tab_container').eq(index).show();
+//     });
+// });
+
 //Tab menu Navigation
 $(document).ready(function(){
     // set defaut active for tab navigation first 
     $('.tab_container:first').show();
-    $('.tab_navigation li:first div').addClass('active');
+    $('.tab_navigation li:first').addClass('active');
 
     // set defaut active for tab navigation was clicked        
     $('.tab_navigation li').click(function(event){
         index  = $(this).index();
-        $('.tab_navigation li div').removeClass('active');
-        $(this).children('div').eq(0).addClass('active');
+        $('.tab_navigation li').removeClass('active');
+        $(this).eq(0).addClass('active');
         $('.tab_container').hide();
         $('.tab_container').eq(index).show();
     });
 });
+
 
 // Back to top
 $(document).ready(function () {

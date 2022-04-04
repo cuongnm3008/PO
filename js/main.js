fdb0a1a29b8b20eb5b4c2eab449acec7f0446c07
DESKTOP-3Q6SPE5\Orenda <cuongnguyenmanh3008@gmail.com>
2022-04-04 09:30:21 +0700

    a

--- js/main.js
+++ js/main.js
@@ -44,8 +44,8 @@ $(document).ready(function () {
               center: true,
               loop: true,
               dots: true,
-            //   autoplay: true,
-            //   autoplayTimeout : 3000,
+              autoplay: true,
+              autoplayTimeout : 3000,
               responsive: {
                   0: {
                       items: 1,
@@ -72,8 +72,8 @@ $(document).ready(function () {
               loop: true,
               dots: false,
               nav: false,
-            //   autoplay: true,
-            //   autoplayTimeout : 3000,
+              autoplay: true,
+              autoplayTimeout : 3000,
               items: 1,
       });
 });
